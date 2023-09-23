dfe330657cb66437e2370bdc53f416071e9e1cd0
Carlos Acosta <carlosalbertoacostaangulo53@gmail.com>
2023-09-23 08:47:48 -0500

    Adaptación de Bootstrap 5 con CDN

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,7 +1,7 @@
 <?php
-  
+
 namespace App\Http\Controllers\Auth;
-  
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,7 +10,7 @@ use App\Models\User;
 use Hash;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
-  
+
 class AuthController extends Controller
 {
     /**
@@ -21,8 +21,8 @@ class AuthController extends Controller
     public function index(): View
     {
         return view('auth.login');
-    }  
-      
+    }
+
     /**
      * Write code on Method
      *
@@ -32,7 +32,7 @@ class AuthController extends Controller
     {
         return view('auth.registration');
     }
-      
+
     /**
      * Write code on Method
      *
@@ -44,35 +44,41 @@ class AuthController extends Controller
             'email' => 'required',
             'password' => 'required',
         ]);
-   
+
+        $validUser = [
+            'email' => 'pepitoperez@gmail.com',
+            'password' => '123456'
+        ];
+        
         $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
+        /* if (Auth::attempt($credentials)) {
             return redirect()->intended('dashboard')
                         ->withSuccess('You have Successfully loggedin');
-        }
-  
+        } */
+
+
         return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
     }
-      
+
     /**
      * Write code on Method
      *
      * @return response()
      */
     public function postRegistration(Request $request): RedirectResponse
-    {  
+    {
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
         ]);
-           
+
         $data = $request->all();
         $check = $this->create($data);
-         
+
         return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
     }
-    
+
     /**
      * Write code on Method
      *
@@ -80,13 +86,13 @@ class AuthController extends Controller
      */
     public function dashboard(): RedirectResponse
     {
-        if(Auth::check()){
+        if (Auth::check()) {
             return view('dashboard');
         }
-  
+
         return redirect("login")->withSuccess('Opps! You do not have access');
     }
-    
+
     /**
      * Write code on Method
      *
@@ -94,13 +100,13 @@ class AuthController extends Controller
      */
     public function create(array $data)
     {
-      return User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => Hash::make($data['password'])
-      ]);
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
     }
-    
+
     /**
      * Write code on Method
      *
@@ -110,7 +116,7 @@ class AuthController extends Controller
     {
         Session::flush();
         Auth::logout();
-  
+
         return Redirect('login');
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel - ItSolutionStuff.com</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <title>Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
     <style type="text/css">
         @import url(https://fonts.googleapis.com/css?family=Raleway:300,400,600);
-  
+        
         body{
             margin: 0;
             font-size: .9rem;
@@ -47,34 +48,34 @@
 </head>
 <body>
     
-<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand" href="#">Laravel</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-   
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                @guest
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('login') }}">Laravel</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    @guest
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                        <a class="nav-link" href="{{ route('register') }}">Registrarse</a>
                     </li>
-                @else
+                    @else
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                        <a class="nav-link" href="{{ route('logout') }}">Salir</a>
                     </li>
-                @endguest
-            </ul>
-  
+                    @endguest
+                </ul>
+                
+            </div>
         </div>
-    </div>
-</nav>
-  
-@yield('content')
-     
+    </nav>
+    
+    @yield('content')
+    
 </body>
 </html>
